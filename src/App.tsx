d5e5ccb23fc79eab36cb0b5652fe5aeae11a7757
ipetropolsky <i.petropolsky@hh.ru>
2024-03-25 22:45:00 +0100

    Setup env

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 const WIDTH = 128;
 const HEIGHT = 70;
@@ -8,11 +8,11 @@ const DX_MOUTH = [12, 0, -10, -10];
 
 const getStyle = (config) => {
     return {
-        width: (WIDTH - 20),
+        width: WIDTH - 20,
         height: HEIGHT,
         ...(config ? { backgroundPosition: `${config.x - 10}px ${config.y}px` } : null),
     };
-}
+};
 
 const eyesConfig = [];
 for (let i = 0; i < 4; i++) {
@@ -20,7 +20,7 @@ for (let i = 0; i < 4; i++) {
         eyesConfig.push({
             x: -WIDTH * i + DX[i],
             y: -HEIGHT * j,
-        })
+        });
     }
 }
 const mouthConfig = [];
@@ -28,8 +28,8 @@ for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 6; j++) {
         mouthConfig.push({
             x: -WIDTH * i + DX_MOUTH[i],
-            y: -389 -HEIGHT * j,
-        })
+            y: -389 - HEIGHT * j,
+        });
     }
 }
 
@@ -41,27 +41,39 @@ function App() {
             <div className="controls">
                 <div className="selection">
                     {eyesConfig.map((eyeConfig) => {
-                        return <div className={`part ${eyeConfig === selectedEyes ? 'part-selected' : ''}`} style={getStyle(eyeConfig)} onClick={() => {
-                            setSelectedEyes(eyeConfig);
-                        }}></div>
+                        return (
+                            <div
+                                className={`part ${eyeConfig === selectedEyes ? 'part-selected' : ''}`}
+                                style={getStyle(eyeConfig)}
+                                onClick={() => {
+                                    setSelectedEyes(eyeConfig);
+                                }}
+                            ></div>
+                        );
                     })}
                 </div>
                 <div className="selection">
                     {mouthConfig.map((mouthConfig) => {
-                        return <div className={`part ${mouthConfig === selectedMouth ? 'part-selected' : ''}`} style={getStyle(mouthConfig)} onClick={() => {
-                            setSelectedMouth(mouthConfig);
-                        }}></div>
+                        return (
+                            <div
+                                className={`part ${mouthConfig === selectedMouth ? 'part-selected' : ''}`}
+                                style={getStyle(mouthConfig)}
+                                onClick={() => {
+                                    setSelectedMouth(mouthConfig);
+                                }}
+                            ></div>
+                        );
                     })}
                 </div>
             </div>
             <div className="result">
                 <div className="emotion">
-                    <div className="part" style={{...getStyle(selectedEyes)}}></div>
-                    <div className="part" style={{...getStyle(selectedMouth)}}></div>
+                    <div className="part" style={{ ...getStyle(selectedEyes) }}></div>
+                    <div className="part" style={{ ...getStyle(selectedMouth) }}></div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
